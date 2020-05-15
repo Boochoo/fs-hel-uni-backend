2988a32f7d47f21a87914bdb8b56103308b9afcc
Ermias Hailemicheal <Boochoo@users.noreply.github.com>
2020-05-15 20:39:30 +0300

    move env viriable to destructor

--- blog-list-app/utils/config.js
+++ blog-list-app/utils/config.js
@@ -1,11 +1,9 @@
 require('dotenv').config()
 
-const { NODE_ENV, TEST_MONGODB_URI, MONGODB_URL } = process.env
+const { NODE_ENV, TEST_MONGODB_URI, MONGODB_URL, SECRET } = process.env
 
 let PORT = process.env.PORT || 3003
 let MONGODB_URI = NODE_ENV === 'test' ? TEST_MONGODB_URI : MONGODB_URL
-
-const SECRET = process.env.SECRET
 
 module.exports = {
   MONGODB_URI,
